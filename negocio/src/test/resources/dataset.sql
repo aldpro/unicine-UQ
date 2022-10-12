0ef89a70bd64df9df16e9238e41799463fbdcc1a
cristian <cristians.barreram@uqvirtual.edu.co>
2022-10-12 17:46:42 -0500

    He adicionado mas datos de test, falta la mitad

--- negocio/src/test/resources/dataset.sql
+++ negocio/src/test/resources/dataset.sql
@@ -1,12 +1,90 @@
-insert into cliente values (1,"pepe@hotmail.net","Pepe","CJT30MNQ1DV",1,"https://instagram.com");
-insert into cliente values (2,"juan@outlook.couk","Juan","HRF52CVT7SO",0,"http://naver.com");
-insert into cliente values (3,"luis@yahoo.couk","Luis","KUL17OKT1SM",0,"http://baidu.com");
-insert into cliente values (4,"maria@icloud.net","Maria","DSS57UGC3QK",1,"http://bbc.co.uk");
-insert into cliente values (5,"luisa@google.couk","Luisa","MQG82VTF2OB",0,"https://naver.com");
+insert into administrador values (1007569040,"cristian@gmail.com","Cristian","52t30XzA2dU");
 
-insert into cliente_telefonos values (1, "32123123");
-insert into cliente_telefonos values (1, "34343123");
-insert into cliente_telefonos values (2, "23423235");
-insert into cliente_telefonos values (3, "98675344");
-insert into cliente_telefonos values (4, "13223445");
-insert into cliente_telefonos values (5, "21333123");
+insert into administrador_teatro values (1119000000,"jhon@yahoo.com","Jhon","02T3CXzA2dX");
+insert into administrador_teatro values (1228000000,"henry@outlook.com","Henry","ESEq2BwzB0rH");
+insert into administrador_teatro values (1337000000,"valentina@gmail.com","Valentina","IRe05CXzA7dX");
+insert into administrador_teatro values (1446000000,"farit@gmail.com","Farit","69YuNC01u6Eq");
+
+insert into ciudad values (1,"Armenia");
+insert into ciudad values (2,"Pereira");
+insert into ciudad values (3,"Cali");
+insert into ciudad values (4,"Bogota");
+insert into ciudad values (5,"Choco");
+
+insert into cliente values (1009000011,"pepe@hotmail.com","Pepe","CjT30mNQ1dV",1,"https://pepe.jpg");
+insert into cliente values (1008000022,"juan@outlook.com","Juan","HrF52cVT7So",0,"http://juan.jpg");
+insert into cliente values (1007000033,"luis@yahoo.com","Luis","kUL17Okt1SM",0,"http://luis.png");
+insert into cliente values (1006000044,"maria@gmail.com","Maria","Dss57UgC3QK",1,"http://maria.jpg");
+insert into cliente values (1005000055,"luisa@google.com","Luisa","MqG82VtF2Ob",0,"https://luis.png");
+
+insert into cliente_telefonos values (1009000011, "3146832477");
+insert into cliente_telefonos values (1009000011, "3008245984");
+insert into cliente_telefonos values (1008000022, "3176857415");
+insert into cliente_telefonos values (1007000033, "3126845287");
+insert into cliente_telefonos values (1006000044, "3139847645");
+insert into cliente_telefonos values (1005000055, "3101036478");
+
+insert into confiteria values (1, "Perro Caliente + Gaseosa", 24000, "C://perroygaseosa.png");
+insert into confiteria values (2, "Crispeta + Gaseosa", 29900, "C://crispetaygaseosa.png");
+insert into confiteria values (3, "Crispeta + Gaseosa para Pareja", 54800, "C://crispetaygaseosa.png");
+insert into confiteria values (4, "Crispeta", 15000, "C://crispeta.png");
+insert into confiteria values (5, "Bebida", 6000, "C://gaseosa.png");
+
+insert into distribucion_silla values (1, 10, 8, 80, "C://esquema[floresta-bosa].html");
+insert into distribucion_silla values (2, 12, 10, 120, "C://esquma[atlantis].jpg");
+insert into distribucion_silla values (3, 8, 8, 64, "C://esquema[multiplaza].jpg");
+insert into distribucion_silla values (4, 9, 8, 72, "C://esquema[colonia].png");
+insert into distribucion_silla values (5, 11, 10, 120, "C://esquema[imperial].jpg");
+
+insert into funcion values (1, 7000, 1, 1, 6);
+insert into funcion values (2, 6500, 2, 2, 5);
+insert into funcion values (3, 6800, 3, 3, 4);
+insert into funcion values (4, 6800, 4, 4, 3);
+insert into funcion values (5, 7100, 5, 5, 2);
+insert into funcion values (6, 6800, 6, 4, 1);
+
+insert into genero values (1, "Terror");
+insert into genero values (2, "Accion");
+insert into genero values (3, "Comedia");
+insert into genero values (4, "Deporte");
+insert into genero values (5, "Animado");
+insert into genero values (6, "Fantasia");
+
+insert into horario values (1, "2022-12-20 23:15:32", "2022-12-20 21:30:32");
+insert into horario values (2, "2022-12-15 20:31:05", "2022-12-15 18:50:45");
+insert into horario values (3, "2022-12-16 24:07:20", "2022-12-16 22:12:02");
+insert into horario values (4, "2022-12-17 19:00:23", "2022-12-17 17:30:06");
+insert into horario values (5, "2022-12-22 20:30:18", "2022-12-22 18:40:28");
+insert into horario values (6, "2022-12-17 23:11:02", "2022-12-17 21:40:12");
+
+insert into pelicula values (1, "Cartelera", "Pinocho", "Tom Hanks : Cynthia Erivo : Benjamin Evan : Luke Evans : Joseph Gordon : Kegan Michael : Giuseppe Bautista : Sheila Atim.", "En un pueblo italiano, el títere de madera Pinocho cobra vida gracias al Hada Azul. Pinocho se esfuerza por comportarse como un niño de carne y hueso, pero su vida da un vuelco al abandonar a su padre para unirse a un circo.", "https://pinocho.png", "https://youtube//wQls8fn.com");
+insert into pelicula values (2, "Preventa", "Dragon Ball: Super Hero", "Masako Nozawa : Hiroshi Kamiya : Mamoru Miyano Aya Hisakawa : Ota Pile : Charles Martinet : Mayumi Tanaka : Sean Shemmel.", "La malvada organización Red Ribbon Army se reforma con nuevos y más poderosos androides, Gamma {1} y Gamma {2} para buscar venganza.", "https://dragonbll.jpg", "https://youtube//SxtZ8fU.com");
+insert into pelicula values (3, "Preventa", "Smile", "Sosie Bacon : Caitlin Stasey : Jessie Usher : Kyle Galllner : Rob Morgan : Nick Arapoglou : Dora Kiss : Robin Weigert : Gillian Zinser.", "Después de ser testigo de un extraño y traumático accidente que involucró a una paciente, la Dr. Rose Cotter (Sosie Bacon) empieza a experimentar sucesos aterradores que no puede explicarse. A medida que el terror comienza a apoderarse de su vida, Rose debe enfrentarse a su pasado para sobrevivir y escapar de su horrible nueva realidad.", "https://smile.html", "https://youtube//Ab4e8fI.com");
+insert into pelicula values (4, "Cartelera", "Minions", "Steve Carell : Pierre Coffin : Sandra Bullock : Steve Coogan : Jon Hamm : Bob Bergen : Michael Keaton : Michael Beattie.", "En los años 70, Gru crece siendo un gran admirador de <<Los salvajes seis>>, un supergrupo de villanos. Para demostrarles que puede ser malvado, Gru idea un plan con la esperanza de formar parte de la banda. Por suerte, cuenta con la ayuda de sus fieles seguidores, los Minions, siempre dispuestos a sembrar el caos.", "https://minions(2022).jpg", "https://youtube//QsrT0jE.com");
+insert into pelicula values (5, "Cartelera", "Encanto", "Stephanie Beatriz : Wilmer Valderrama : John Leguizamo : Alan Tudyk : Sarah Nicole : Maluma : Diane Guerrero : Jessica Darrow.", "En lo alto de las montañas de Colombia hay un lugar encantado llamado Encanto. Aquí, en una casa mágica, vive la extraordinaria familia Madrigal donde todos tienen habilidades fantásticas.", "https://encantio.png", "https://youtube//wQls8fn.com");
+
+insert into pelicula_generos values (1, 6);
+insert into pelicula_generos values (1, 5);
+insert into pelicula_generos values (2, 2);
+insert into pelicula_generos values (2, 5);
+insert into pelicula_generos values (3, 1);
+insert into pelicula_generos values (4, 3);
+insert into pelicula_generos values (4, 5);
+insert into pelicula_generos values (5, 6);
+
+insert into sala values (1, "Atlantis", 2, 5);
+insert into sala values (2, "FLoresta", 1, 5);
+insert into sala values (3, "Gran Plaza Bosa", 1, 4);
+insert into sala values (4, "Multiplaza", 3, 3);
+insert into sala values (5, "Parque Colonia", 4, 2);
+insert into sala values (6, "Plaza Imperial", 5, 1);
+
+insert into teatro values (1, "Carrera 14 # 4-6 Norte", "3185469257", 1119000000, 5);
+insert into teatro values (2, "Calle 3 # 1 A 24 Sur", "3185749321", 1119000000, 4);
+insert into teatro values (3, "Calle 16 4-2 Centro", "3124720846", 1228000000, 4);
+insert into teatro values (4, "Carrera 7 # B 12-13 Sur", "3001247585", 1337000000, 3);
+insert into teatro values (5, "Carrera 9 # 4-7 Oeste", "3186347896", 1446000000, 2);
+insert into teatro values (6, "Calle 4 # 4-2 Sur", "3178532410", 1446000000, 1);
+
+
+

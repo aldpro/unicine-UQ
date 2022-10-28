70aeb4e737afa159d6ed8008b3f7a3106a216cf0
MafeG09 <108835794+MafeG09@users.noreply.github.com>
2022-10-28 01:27:33 -0500

    Configuracion JSF
    Se dio inicio al modulo web

---
+++ web/src/main/java/co/edu/uniquindio/unicine/bean/InicioBean.java
@@ -0,0 +1,17 @@
+package co.edu.uniquindio.unicine.bean;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.stereotype.Component;
+
+import javax.faces.view.ViewScoped;
+import java.io.Serializable;
+
+@Component
+@ViewScoped
+public class InicioBean implements Serializable {
+
+    @Getter
+    @Setter
+    private String mensaje = "Mi primera pagina en JSF";
+}

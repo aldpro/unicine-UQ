6c617a47ee5c3a7eb80f0db70e26690b753b8291
MafeG09 <108835794+MafeG09@users.noreply.github.com>
2022-11-21 22:57:23 -0500

    Datos iniciales v2

--- web/src/main/java/co/edu/uniquindio/unicine/config/DatosIniciales.java
+++ web/src/main/java/co/edu/uniquindio/unicine/config/DatosIniciales.java
@@ -37,6 +37,12 @@ public class DatosIniciales implements CommandLineRunner {
             Ciudad c4 = Ciudad.builder().nombre("Bogota").build();
             Ciudad c5 = Ciudad.builder().nombre("Choco").build();
 
+            adminServicio.crearCiudad(c1);
+            adminServicio.crearCiudad(c2);
+            adminServicio.crearCiudad(c3);
+            adminServicio.crearCiudad(c4);
+            adminServicio.crearCiudad(c5);
+
             AdministradorTeatro adminTeatro1 = AdministradorTeatro.builder().cedula(1228000000).nombre("Maria").correo("mariaf.camachog@uqvirtual.edu.co").password("1234").build();
             AdministradorTeatro adminTeatro2 = AdministradorTeatro.builder().cedula(1119000000).nombre("Jhon").correo("jhona.belloc@uqvirtual.edu.co").password("1234").build();
             AdministradorTeatro adminTeatro3 = AdministradorTeatro.builder().cedula(1337000000).nombre("Cristian").correo("cristians.barreram@uqvirtual.edu.co").password("1234").build();
@@ -44,8 +50,16 @@ public class DatosIniciales implements CommandLineRunner {
             AdministradorTeatro adminTeatro5 = AdministradorTeatro.builder().cedula(1557000000).nombre("Rodolfo").correo("rodolfo@email.com").password("1234").build();
             AdministradorTeatro adminTeatro6 = AdministradorTeatro.builder().cedula(1657000800).nombre("Jose").correo("jose@email.com").password("1234").build();
 
+            adminServicio.crearAdminTeatro(adminTeatro1);
+            adminServicio.crearAdminTeatro(adminTeatro2);
+            adminServicio.crearAdminTeatro(adminTeatro3);
+            adminServicio.crearAdminTeatro(adminTeatro4);
+            adminServicio.crearAdminTeatro(adminTeatro5);
+            adminServicio.crearAdminTeatro(adminTeatro6);
 
             Administrador admin1 = Administrador.builder().nombre("Henry").correo("henrya.barraganp@uqvirtual.edu.co").password("1234").build();
+
+            adminServicio.crearAministrador(admin1);
 
             Pelicula peli1 = Pelicula.builder().estado(EstadoPelicula.valueOf("PREVENTA")).nombre("Pinocho")
                     .puntuacion(4.2f).reparto("Tom Hanks, Cynthia Erivo, Luke Evans").
@@ -67,10 +81,19 @@ public class DatosIniciales implements CommandLineRunner {
                     .sinopsis("En lo alto de las montañas de Colombia hay un lugar encantado llamado Encanto. Aquí, en una casa mágica, vive la extraordinaria familia Madrigal donde todos tienen habilidades fantásticas.")
                     .urlTrailer("https://www.youtube.com/embed/SAH_W9q_brE").build();
 
+            adminServicio.crearPelicula(peli1);
+            adminServicio.crearPelicula(peli2);
+            adminServicio.crearPelicula(peli3);
+            adminServicio.crearPelicula(peli4);
+            adminServicio.crearPelicula(peli5);
+
             Cupon cup1 = Cupon.builder().descripcion( "Cupon del 15% de descuento por registrarse por primera vez en nuestra plataforma").descuento(15.0f)
                     .criterio("Primer registro").fechaVencimiento(LocalDateTime.parse("2022-12-25T20:00:00")).build();
             Cupon cup2 = Cupon.builder().descripcion("Cupon del 10% de descuento por realizar una primera compra por medio de nuestra plataforma")
                     .descuento(10.0f).criterio("Primera compra").fechaVencimiento(LocalDateTime.parse("2022-12-19T15:45:00")).build();
+
+            adminServicio.crearCupon(cup1);
+            adminServicio.crearCupon(cup2);
 
             Confiteria conf1 = Confiteria.builder().nombre("Combo para niños").precio(15000f)
                     .urlImagen("https://res.cloudinary.com/dwu4xtiun/image/upload/v1667927564/unicine/confiteria/combo_ni%C3%B1os_ydpbay.jpg").build();
@@ -83,6 +106,11 @@ public class DatosIniciales implements CommandLineRunner {
             Confiteria conf5 = Confiteria.builder().nombre("Nevado de arequipe").precio(6000f)
                     .urlImagen("https://res.cloudinary.com/dwu4xtiun/image/upload/v1667927565/unicine/confiteria/nevado_arequipe_afpfeo.jpg").build();
 
+            adminServicio.crearConfiteria(conf1);
+            adminServicio.crearConfiteria(conf2);
+            adminServicio.crearConfiteria(conf3);
+            adminServicio.crearConfiteria(conf4);
+            adminServicio.crearConfiteria(conf5);
 
             Teatro teatro1 = Teatro.builder().direccion("Carrera 14 # 4-6 Norte").telefono("3185469257").ciudad(c5).administradorTeatro(adminTeatro2).build();
             Teatro teatro2 = Teatro.builder().direccion("Calle 3 # 1 A 24 Sur").telefono("3185749321").ciudad(c4).administradorTeatro(adminTeatro1).build();
@@ -90,6 +118,13 @@ public class DatosIniciales implements CommandLineRunner {
             Teatro teatro4 = Teatro.builder().direccion("Carrera 7 # B 12-13 Sur").telefono("3001247585").ciudad(c3).administradorTeatro(adminTeatro4).build();
             Teatro teatro5 = Teatro.builder().direccion("Carrera 9 # 4-7 Oeste").telefono("3186347896").ciudad(c2).administradorTeatro(adminTeatro5).build();
             Teatro teatro6 = Teatro.builder().direccion("Calle 4 # 4-2 Sur").telefono("3178532410").ciudad(c1).administradorTeatro(adminTeatro6).build();
+
+            adminTeatroServicio.crearTeatro(teatro1);
+            adminTeatroServicio.crearTeatro(teatro2);
+            adminTeatroServicio.crearTeatro(teatro3);
+            adminTeatroServicio.crearTeatro(teatro4);
+            adminTeatroServicio.crearTeatro(teatro5);
+            adminTeatroServicio.crearTeatro(teatro6);
 
             Sala sala1 = Sala.builder().nombre("Atlantis").tipoSala(TipoSala.XD).teatro(teatro5).build();
             Sala sala2 = Sala.builder().nombre("FLoresta").tipoSala(TipoSala.NORMAL).teatro(teatro5).build();
@@ -100,6 +135,15 @@ public class DatosIniciales implements CommandLineRunner {
             Sala sala7 = Sala.builder().nombre("Plaza Imperial").tipoSala(TipoSala.DX4).teatro(teatro1).build();
             Sala sala8 = Sala.builder().nombre("Colon").tipoSala(TipoSala.IMAX).teatro(teatro6).build();
 
+            adminTeatroServicio.crearSala(sala1);
+            adminTeatroServicio.crearSala(sala2);
+            adminTeatroServicio.crearSala(sala3);
+            adminTeatroServicio.crearSala(sala4);
+            adminTeatroServicio.crearSala(sala5);
+            adminTeatroServicio.crearSala(sala6);
+            adminTeatroServicio.crearSala(sala7);
+            adminTeatroServicio.crearSala(sala8);
+
             Horario hora1 = Horario.builder().fechaFin(LocalDate.parse("2022-12-20")).fechaInicio(LocalDate.parse("2022-12-20")).hora("20:00").build();
             Horario hora2 = Horario.builder().fechaFin(LocalDate.parse("2022-12-15")).fechaInicio(LocalDate.parse("2022-12-15")).hora("21:00").build();
             Horario hora3 = Horario.builder().fechaFin(LocalDate.parse("2022-12-16")).fechaInicio(LocalDate.parse("2022-12-16")).hora("22:00").build();
@@ -107,6 +151,12 @@ public class DatosIniciales implements CommandLineRunner {
             Horario hora5 = Horario.builder().fechaFin(LocalDate.parse("2022-12-22")).fechaInicio(LocalDate.parse("2022-12-22")).hora("20:00").build();
             Horario hora6 = Horario.builder().fechaFin(LocalDate.parse("2022-12-17")).fechaInicio(LocalDate.parse("2022-12-17")).hora("20:00").build();
 
+            adminTeatroServicio.crearHorario(hora1);
+            adminTeatroServicio.crearHorario(hora2);
+            adminTeatroServicio.crearHorario(hora3);
+            adminTeatroServicio.crearHorario(hora4);
+            adminTeatroServicio.crearHorario(hora5);
+            adminTeatroServicio.crearHorario(hora6);
 
             Funcion funcion1 = Funcion.builder().precio(7000f).horario(hora1).pelicula(peli1).sala(sala6).build();
             Funcion funcion2 = Funcion.builder().precio(6500f).horario(hora2).pelicula(peli2).sala(sala5).build();
@@ -115,6 +165,14 @@ public class DatosIniciales implements CommandLineRunner {
             Funcion funcion5 = Funcion.builder().precio(7100f).horario(hora5).pelicula(peli5).sala(sala2).build();
             Funcion funcion6 = Funcion.builder().precio(6800f).horario(hora6).pelicula(peli4).sala(sala1).build();
             Funcion funcion7 = Funcion.builder().precio(10000f).horario(hora1).pelicula(peli3).sala(sala8).build();
+
+            adminTeatroServicio.crearFuncion(funcion1);
+            adminTeatroServicio.crearFuncion(funcion2);
+            adminTeatroServicio.crearFuncion(funcion3);
+            adminTeatroServicio.crearFuncion(funcion4);
+            adminTeatroServicio.crearFuncion(funcion5);
+            adminTeatroServicio.crearFuncion(funcion6);
+            adminTeatroServicio.crearFuncion(funcion7);
 
         }
     }
